2dcd3f463651c0433855f7b0f93cc2381c8407c2
Marcelo <marcelo.guzman.g@usach.cl>
2026-03-15 21:41:25 -0300

    feat: add tfvars file example

--- infra/data/variables.tf
+++ infra/data/variables.tf
@@ -9,10 +9,9 @@ variable "region" {
   default     = "southamerica-west1"
 }
 
-variable "zone" {
-  description = "The default compute zone"
+variable "gcs_bucket_name" {
+  description = "The name of the BigQuery dataset"
   type        = string
-  default     = "southamerica-west1-a"
 }
 
 variable "data_location" {
@@ -20,13 +19,6 @@ variable "data_location" {
   type        = string
   default     = "southamerica-west1"
 }
-
-
-variable "gcs_bucket_name" {
-  description = "The name of the BigQuery dataset"
-  type        = string
-}
-
 
 variable "bq_dataset_id" {
   description = "The name of the BigQuery dataset"
